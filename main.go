6a84e8af962fa0666c0c4fc4bf19ff38ab168a53
158 <7408037+gofer@user.noreply.gitee.com>
2024-06-07 08:52:14 +0800

    修改/返回内容

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,11 +9,10 @@ func main() {
 	server := gin.Default()
 
 	// 2. 定义路由：当使用 GET 方法访问路径为 /hello 时，执行回调函数
-	server.GET("/hello", func(c *gin.Context) {
-		// 3. 在回调函数中，返回一个字符串 "hello, go" 并设置HTTP状态码为200 OK
-		c.String(http.StatusOK, "hello, go")
+	server.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, Cloudflare Page!")
 	})
 
 	// 4. 启动服务，监听在 0.0.0.0:8080 上
-	server.Run(":8080") // 如果不指定端口号，默认为8080
+	server.Run(":80") // 如果不指定端口号，默认为8080
 }
